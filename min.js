e38c82db5e4e5e315365bd3c06792de0dc7e256f
M.M <57150476+Mohammadreza4mn@users.noreply.github.com>
2020-05-21 19:23:16 +0430

    A little update

--- min.js
+++ min.js
@@ -1,46 +1,12 @@
-function addcode(event){
-  var input, ul, li, result, i, txtValue, regex, regex2, keyboard, error;
+var input, ul, li, result, i, txtValue, regex, regex2, keyboard, error;
 error=document.getElementById('error');
 input = document.getElementById("input").value;
 ul = document.getElementById("products");
 li = ul.getElementsByTagName("li");
 regex=/[^ا-ی\s]/;
 regex2=["تابلو" , "ساعت" ,"جاکفشی" , "صندلی" ,"مبل" , "پرده" ,"کتابخانه" , "دراور" ,"شلف" , "لوازم تزیینی" ,"میز"];
-// اضافه کردن رویداد به دکمه های کیبورد
-keyboard=event.keyCode;
-    if (keyboard == 13){
-if(input==""){
-  error.innerHTML="😶خالی است😶<br>هنوز چیزی در قسمت بالا ننوشتی که برات جستجو کنم"
-}
-else if(regex.test(input)){
-  error.innerHTML="✍️فارسی بنویس✍️<br>بیا با کمک همدیگه زبان فارسی رو گسترش بدیم"
-}
-else if(regex2.indexOf(input)==-1){
-  error.innerHTML="🏃این محصول داریم اضافه می‌کنیم🏃<br>اما تا اون موقع پیشنهاد میکنم یه نگاهی به دسته بندی محصولات بندازی<br><a href='https://www.google.com/search?q=محصولات دکوراسیون و مبلمان داخلی'><button>مشاهده دسته بندی محصولات</button></a>"
-}
-else{
-for (i = 0; i < li.length; i++) {
-  result = li[i].getElementsByTagName("a")[0];
-    txtValue = result.textContent || result.innerText;
-    if (txtValue.indexOf(input) > -1) {
-      window.location=result
-    }
-    else{
-      error.innerHTML="✔️پیش به سوی یه خرید باحال✔️"
-    };
-};
 
-    };
-};
-};
 function search(){
-  var input, ul, li, result, i, txtValue, regex, regex2, error;
-error=document.getElementById('error');
-input = document.getElementById("input").value;
-ul = document.getElementById("products");
-li = ul.getElementsByTagName("li");
-regex=/[^ا-ی\s]/;
-regex2=["تابلو" , "ساعت" ,"جاکفشی" , "صندلی" ,"مبل" , "پرده" ,"کتابخانه" , "دراور" ,"شلف" , "لوازم تزیینی" ,"میز"];
 if(input==""){
   error.innerHTML="😶خالی است😶<br>هنوز چیزی در قسمت بالا ننوشتی که برات جستجو کنم"
 }
@@ -64,3 +30,11 @@ for (i = 0; i < li.length; i++) {
 
     };
 };
+
+function addcode(event){
+  // اضافه کردن رویداد به دکمه های کیبورد
+    keyboard=event.keyCode;
+      if (keyboard == 13){
+        search()
+      };
+  };
